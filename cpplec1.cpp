bc1763083dd84853ca7fbdafe1f4d980c4e5ac86
NishanT <kidnishant@gmail.com>
2025-09-07 16:54:43 +0530

    Delete cpplec1.cpp

--- cpplec1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // header file
-using namespace std;
-
-int main(){ //main function
-    cout<<"My name is  Nishant "<<endl<<"Oraon";  // to print this
-    return 0; // function is end when this is return
-}
